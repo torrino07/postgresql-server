30c6933a417b7b8695e3a5284d9c15b09b20c3a0
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-12 21:39:13 +0200

    tefet

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM postgres
 
 ENV POSTGRES_DB="trading"
-ENV POSTGRES_USER="postgres"
-ENV POSTGRES_PASSWORD="admingres"
+ENV POSTGRES_HOST_AUTH_METHOD="trust"
 ENV PGDATA="/data/postgresql"
+
+COPY pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
+RUN chown postgres:postgres /var/lib/postgresql/data/pg_hba.conf
+COPY postgresql.conf /etc/postgresql/postgresql.conf
 
 COPY sql/*/*.sql /docker-entrypoint-initdb.d/
 
--- docker.local.sh
+++ docker.local.sh
@@ -1,12 +1,17 @@
+DOMAINOWNER="160945804984"
+REGION="us-east-1"
 USER="torrino07"
 
 export IMAGE="postgressql-server"
 export VERSION="latest"
 
-
 docker build -t "$IMAGE" .
 
 docker tag "$IMAGE":"$VERSION" "$USER"/"$IMAGE":"$VERSION"
 docker push "$USER"/"$IMAGE":"$VERSION"
-
 docker run -p 5432:5432 torrino07/postgressql-server:latest
+
+## AWS
+# aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com
+# docker tag "$IMAGE":"$VERSION" "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
+# docker push "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
