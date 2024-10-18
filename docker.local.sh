db5596c386d9837f5f18e28f8f63d03ef4958e4f
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-10-18 22:54:21 +0200

    test

--- docker.local.sh
+++ docker.local.sh
@@ -4,19 +4,19 @@ USER="torrino07"
 
 POSTGRES_DB="trading"
 POSTGRES_HOST_AUTH_METHOD="trust"
-IMAGE="postgressql-server"
+IMAGE="postgresql_server"
 VERSION="latest"
 
 docker build \
-    --build-args "$POSTGRES_DB" \
-    --build-args "$POSTGRES_HOST_AUTH_METHOD" \
+    --build-arg POSTGRES_DB="$POSTGRES_DB" \
+    --build-arg POSTGRES_HOST_AUTH_METHOD="$POSTGRES_HOST_AUTH_METHOD" \
     -t "$IMAGE" .
 
-docker tag "$IMAGE":"$VERSION" "$USER"/"$IMAGE":"$VERSION"
-docker push "$USER"/"$IMAGE":"$VERSION"
-docker run -p 5432:5432 torrino07/postgressql-server:latest
+# docker tag "$IMAGE":"$VERSION" "$USER"/"$IMAGE":"$VERSION"
+# docker push "$USER"/"$IMAGE":"$VERSION"
+# docker run -p 5432:5432 torrino07/postgressql-server:latest
 
 ## AWS
-# aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com
-# docker tag "$IMAGE":"$VERSION" "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
-# docker push "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com
+docker tag "$IMAGE":"$VERSION" "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
+docker push "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
