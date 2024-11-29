4ab8f156f6fa7807099d5e75e2198301446501ad
DGtalledo <61323407+torrino07@users.noreply.github.com>
2024-11-29 23:13:45 +0100

    test

--- docker.local.sh
+++ docker.local.sh
@@ -16,6 +16,7 @@ docker tag "$IMAGE":"$VERSION" "$USER"/"$IMAGE":"$VERSION"
 docker push "$USER"/"$IMAGE":"$VERSION"
 # docker run -p 5432:5432 torrino07/postgressql-server:latest
 
+
 ## AWS
 # aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com
 # docker tag "$IMAGE":"$VERSION" "$DOMAINOWNER".dkr.ecr."$REGION".amazonaws.com/"$IMAGE":"$VERSION"
