dbb6cea232afe006ebcd9e0e0e7c6d9dc693dfeb
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-01-06 23:47:54 +0100

    test

--- sql/tables/05-create-socket-parameters.sql
+++ sql/tables/05-create-socket-parameters.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bots.SocketParameters
 (
-    Host VARCHAR(50),
+    Host VARCHAR(50) NOT NULL,
     Port INT NOT NULL,
     Request JSONB NOT NULL, 
     Handshake VARCHAR(50),
     Channel VARCHAR(50) NOT NULL,
-    Topic VARCHAR(50) NOT NULL, 
+    Topic VARCHAR(50) NOT NULL
 );
