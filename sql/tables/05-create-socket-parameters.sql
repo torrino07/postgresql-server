1f6e7449ef34579a77ee5ef342dc4a5228f5178e
DGtalledo <61323407+torrino07@users.noreply.github.com>
2025-01-07 02:19:36 +0100

    test

--- sql/tables/05-create-socket-parameters.sql
+++ sql/tables/05-create-socket-parameters.sql
@@ -12,6 +12,6 @@ CREATE TABLE bots.Sockets
     CommandId VARCHAR(50) NOT NULL,
     Region VARCHAR(50) NOT NULL,
     Environment VARCHAR(50) NOT NULL,
-    State VARCHAR(50) NOT NULL,
+    ProcessStatus VARCHAR(50) NOT NULL,
     CreatedAt TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3)
 );
